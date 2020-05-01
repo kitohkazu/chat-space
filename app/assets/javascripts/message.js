cb0bb7a7f66b3139be9be945f1ef06746a9dee01
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-05-01 15:44:23 +0900

    add html commit
    ・作成したhtmlをメッセージ画面の一番下に追加する

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,45 @@
 $(function(){
+  function buildHTML (message){
+    if (message.image) {
+      var html = 
+        `<div class="message-list__title">
+          <div class="message-list__title__name">
+            ${message.user_name}
+          </div>
+          <div class="message-list__title__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-list__text">
+          <p class="message-list__text__content">
+            ${message.content}
+          </p>
+          <img class="message-list__text__content" src="${message.image}">
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message-list__title">
+          <div class="message-list__title__name">
+            ${message.user_name}
+          </div>
+          <div class="message-list__title__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-list__text">
+          <p class="message-list__text__content">
+            ${message.content}
+          </p>
+        </div>`
+      return html;
+    };
+  }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -12,6 +48,10 @@ $(function(){
       processData: false,
       contentType: false
     })
-  });
-
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message-list').append(html);
+      $('#message_content').val('');
+    })
+  })
 });
