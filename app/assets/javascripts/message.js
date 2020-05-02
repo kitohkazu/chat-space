39ef8e423a9455c507e5bec74abb012dba86f4cf
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-05-02 23:36:21 +0900

    add display post commit
    ・登校データを表示できるように、最新メッセージのidを取得した。
    ・７秒ごとにリクエストするように、setInterval()関数を実装した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,36 +1,40 @@
 $(function(){
   function buildHTML (message){
     if (message.image) {
-      var html = 
-        `<div class="message-list__title">
-          <div class="message-list__title__name">
-            ${message.user_name}
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message-list__title">
+            <div class="message-list__title__name">
+              ${message.user_name}
+            </div>
+            <div class="message-list__title__date">
+              ${message.created_at}
+            </div>
           </div>
-          <div class="message-list__title__date">
-            ${message.created_at}
+          <div class="message-list__text">
+            <p class="message-list__text__content">
+              ${message.content}
+            </p>
+            <img class="message-list__text__content" src="${message.image}">
           </div>
-        </div>
-        <div class="message-list__text">
-          <p class="message-list__text__content">
-            ${message.content}
-          </p>
-          <img class="message-list__text__content" src="${message.image}">
         </div>`
       return html;
     } else {
       var html =
-        `<div class="message-list__title">
-          <div class="message-list__title__name">
-            ${message.user_name}
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message-list__title">
+            <div class="message-list__title__name">
+              ${message.user_name}
+            </div>
+            <div class="message-list__title__date">
+              ${message.created_at}
+            </div>
           </div>
-          <div class="message-list__title__date">
-            ${message.created_at}
+          <div class="message-list__text">
+            <p class="message-list__text__content">
+              ${message.content}
+            </p>
           </div>
-        </div>
-        <div class="message-list__text">
-          <p class="message-list__text__content">
-            ${message.content}
-          </p>
         </div>`
       return html;
     };
@@ -59,4 +63,33 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
